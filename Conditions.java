4536ff7a2d755e11c0f37e61e08da89790de7007
pavan866 <pavanks7288@gmail.com>
2022-10-08 17:50:40 +0530

    pavan

---
+++ Conditions.java
@@ -0,0 +1,26 @@
+package javaa;
+
+public class Conditions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =15;
+				{
+		switch(i) {
+		case 15:
+			System.out.println("you are  not elligble to vote ");
+			break;
+		case 21:
+			System.out.println("you are ellible to vote");
+		    break;
+		case 18:
+			System.out.println("default statement");
+		}
+
+	} 
+				
+				
+				
+
+	}
+}
