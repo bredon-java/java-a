0b33daa9b28767e89be3dd871eb93118fae5cd85
manikumar123-monstar <113438333+manikumar123-monstar@users.noreply.github.com>
2022-10-10 16:36:25 +0530

    Odd_numbercode

---
+++ Odd_number.java
@@ -0,0 +1,15 @@
+package manicom.src;
+
+public class Odd_number {
+
+	public static void main(String[] args) {
+		
+		for(int n=1;n<=50;n++) {
+			if(!(n%2==0)) {
+				System.out.println(n+"");
+			}
+		}
+
+	}
+
+}
