0212b979de25336892d444dd46f933d0d1147331
ajaydatti <114554402+ajaydatti@users.noreply.github.com>
2022-10-10 16:41:20 +0530

    odd_number code

--- Odd_number.java
+++ Odd_number.java
@@ -1,13 +1,14 @@
-package manicom.src;
+package com.practise;
 
 public class Odd_number {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=0;
 		
-		for(int n=1;n<=50;n++) {
-			if(!(n%2==0)) {
-				System.out.println(n+"");
-			}
+		for(int i=0; i<=50; i++) {
+			if(i%2==1)
+				System.out.println("odd number are "+ i);
 		}
 
 	}
